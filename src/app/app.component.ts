452966c9cf53bce3e11603faa142cef3a1ec8a65
ByAndrey <andbykov@hotmail.com>
2021-01-16 17:50:36 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,10 @@ import { ActivatedRoute } from '@angular/router';
 export class AppComponent implements OnInit {
 
 	dataset: any =[];
-	avg_dataset: any[] =[];
+	avg_iops_dataset: any[] =[];
+	avg_bw_dataset: any[] =[];	
 	lat_labels: any[] =[];
-	logs: any[] = [];
+	logs: any[] =[];
 	logs_list: string ="";
 
 constructor(private httpClient: HttpClient, private activatedRoute: ActivatedRoute) {}
@@ -32,30 +33,35 @@ ngOnInit() {
 	//   if (this.logs){
 	this.logs_list="";
 	//   this.logs.forEach(element => {
-	if (logs){
-	logs.forEach( element => {
+	if (this.logs){
+	this.logs.forEach( element => {
 	      this.logs_list=this.logs_list+'logs='+element+'&';
 		  this.httpClient.get('http://192.168.0.67:5010/time_log_json?'+this.logs_list).subscribe(dataset => {
 			this.dataset = dataset;
-			this.avg_dataset=[];
+			this.avg_iops_dataset=[];
 			this.lat_labels=[];
-			// console.log("here is:");
-			// console.log('http://192.168.0.67:5010/time_log_json?'+this.logs_list);
+			this.avg_bw_dataset=[]
+
 			this.dataset.forEach(element => {
+				if ((element['group'].includes('_iops-')) || (element['group'].includes('_lat-'))){
 				if (element['x']%10==0){
-					this.avg_dataset.push(element)
-				}
+					this.avg_iops_dataset.push(element)
+				}}
+				if ((element['group'].includes('_bw-')) || (element['group'].includes('_lat-'))){
+					if (element['x']%10==0){
+						this.avg_bw_dataset.push(element)
+				}}
 			});
 			
 			this.dataset.forEach(element => {
-				if (element['group'].includes('lat')){
+				if (element['group'].includes('_lat-')){
 					if (!(this.lat_labels.includes(element['group']))) {
 					this.lat_labels.push(element['group']);}
 				}
 			});
 			
 			this.options = {
-				 "title": "FIO_Parser",
+				 "title": "IOPS",
 				 "axes": {
 						"bottom": {
 								 "title": "Time",
@@ -74,8 +80,31 @@ ngOnInit() {
 						 }
 				 },
 				 "curve": "curveNatural",
-				 "height": "400px"
+				 "height": "350px"
 			 };
+			this.options_bw = {
+				"title": "BW",
+				"axes": {
+					   "bottom": {
+								"title": "Time",
+								"mapsTo": "x",
+								"scaleType": "linear"
+						},
+						"left": {
+								"title": "MB/sec",
+								"mapsTo": "y",
+								"scaleType": "linear"
+						},
+						"right": {
+								"title": "Lat",
+								"mapsTo": "z",
+								"correspondingDatasets": this.lat_labels,								 
+						}
+				},
+				"curve": "curveNatural",
+				"height": "350px"
+			};
+
 		  });	   
 	   });}
 	
@@ -83,13 +112,19 @@ ngOnInit() {
 }
 
 onInputChange(event: any) {
-	this.avg_dataset=[]
+	this.avg_iops_dataset=[];
+	this.avg_bw_dataset=[];
 	this.dataset.forEach(element => {
-		if (element['x']%event==0){
-			this.avg_dataset.push(element)
-		}
+		if ((element['group'].includes('_iops-')) | (element['group'].includes('_lat-'))){
+			if (element['x']%event==0){
+				this.avg_iops_dataset.push(element)
+			}}
+		if ((element['group'].includes('_bw-')) | (element['group'].includes('_lat-'))){
+				if (element['x']%event==0){
+					this.avg_bw_dataset.push(element)
+			}}
 	});
-	return(this.avg_dataset)
+	return(this.avg_iops_dataset)
   }
 
   options = {
@@ -112,7 +147,29 @@ onInputChange(event: any) {
 			}
 	},
 	"curve": "curveNatural",
-	"height": "400px"
+	"height": "350px"
+};
+options_bw = {
+	"title": "BW",
+	"axes": {
+			"bottom": {
+					"title": "Time",
+					"mapsTo": "x",
+					"scaleType": "linear"
+			},
+			"left": {
+					"title": "IOPS",
+					"mapsTo": "y",
+					"scaleType": "linear"
+			},
+			"right": {
+					"title": "Lat",
+					"mapsTo": "z",
+					"correspondingDatasets": this.lat_labels,
+			}
+	},
+	"curve": "curveNatural",
+	"height": "350px"
 };
 
 }
